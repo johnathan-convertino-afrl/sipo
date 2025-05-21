ed7b7029c7006ccd69939cd015a9102ea36bc5f1
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-05-20 20:56:06 -0400

    update names and test actions

--- src/sipo.v
+++ src/sipo.v
@@ -73,7 +73,7 @@ module sipo #(
     `include "util_helper_math.vh"
 
     localparam CK_COUNT_AMOUNT = (COUNT_AMOUNT > BUS_WIDTH*8 ? BUS_WIDTH*8 : (COUNT_AMOUNT == 0 ? BUS_WIDTH*8 : COUNT_AMOUNT));
-    
+
     // makes life easier, calculate number of bits needed for count register
     localparam COUNT_WIDTH = clogb2(CK_COUNT_AMOUNT)+1;
 
